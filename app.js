e7726928770ec33b09848ae507d6a1b9a5bb189a
Asai <Asai_Code@Asais-MacBook-Pro.local>
2017-12-29 19:51:27 -0800

    Second App

---
+++ app.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('#btn');
+var update = document.querySelector('#update');
+
+btn.addEventListener('click', function () {
+    if (update.style.display === 'none') {
+      update.style.display = 'block';
+    } else {
+      update.style.display = 'block';
+    }
+});
